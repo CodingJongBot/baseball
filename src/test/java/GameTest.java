e81e3614bd442f0c3b78ab37d316348ea4483326
jongwon <oasis9311@gmail.com>
2025-06-04 13:44:53 +0900

    [feature] page24

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,5 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
+
+    @Test
+    void name() {
+        Game game = new Game();
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game game = new Game();
+            game.guess(null);
+        });
+    }
 }
