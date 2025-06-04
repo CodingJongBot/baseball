358b9739aedb5c5c435d0167982def85faa8b1cc
jongwon <oasis9311@gmail.com>
2025-06-04 13:47:59 +0900

    [feature] page28

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -23,4 +23,13 @@ class GameTest {
             game.guess(null);
         });
     }
+
+
+    @Test
+    void throwExceptionWhenInputLengthIsUnmatched() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess("12");
+        });
+    }
+
 }
